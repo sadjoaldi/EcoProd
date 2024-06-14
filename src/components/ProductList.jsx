79756efbf20a0e1dec748884f6165319d5a3ca03
sadjoaldi <alsannealdi@hotmail.com>
2024-06-14 14:32:36 +0200

    useeffect et localstorage mis en place

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -8,22 +8,17 @@ function ProductList() {
   // la fonction qui permet l'ajout de produit à la liste
   const handleAddProduct = (newProduct) => {
     setProducts([...products, newProduct]);
+
+    // le localstorage permet de stocker les produits
+    localStorage.setItem("products", JSON.stringify([...products, newProduct]));
   };
 
   // useEffect pour recuperer les produits stockés dans le local storage lors du chargment de la page
 
-  // avoir problème de rendu
   useEffect(() => {
     const proSaveList = localStorage.getItem("products");
-    if (proSaveList) {
-      setProducts(JSON.parse(proSaveList));
-    }
+    if (proSaveList) setProducts(JSON.parse(proSaveList));
   }, []);
-
-  //ce useEffect permet de stocker les produits dans le local storage lorsqu'on ajoute un nouveau produit
-  useEffect(() => {
-    localStorage.setItem("products", JSON.stringify(products));
-  }, [products]);
 
   return (
     <>
