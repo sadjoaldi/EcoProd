0e4512e1d55f7a6f631513f363c66c2000f0704a
sadjoaldi <alsannealdi@hotmail.com>
2024-06-14 14:12:18 +0200

    useeffect pour stocker les produits en local, rest le useeffect pour afficher les produits stocker

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Product from "./Product";
 import ProductForm from "./ProductForm";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
 
+  // la fonction qui permet l'ajout de produit à la liste
   const handleAddProduct = (newProduct) => {
     setProducts([...products, newProduct]);
   };
+
+  // useEffect pour recuperer les produits stockés dans le local storage lors du chargment de la page
+
+  // avoir problème de rendu
+  useEffect(() => {
+    const proSaveList = localStorage.getItem("products");
+    if (proSaveList) {
+      setProducts(JSON.parse(proSaveList));
+    }
+  }, []);
+
+  //ce useEffect permet de stocker les produits dans le local storage lorsqu'on ajoute un nouveau produit
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(products));
+  }, [products]);
 
   return (
     <>
